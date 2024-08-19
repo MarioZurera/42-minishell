267fa5473982b2924287adabe3360a5428a05cc8
mzurera- <mzurera-@student.42malaga.com>
2024-08-19 18:59:39 +0200

    feat(utils): Added ft_isnumber.

---
+++ minishell/utils/ft_isnumber.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isnumber.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aflorido <aflorido@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/11 14:39:22 by aflorido          #+#    #+#             */
+/*   Updated: 2024/08/05 19:15:45 by aflorido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ *
+ * @param str A number is valid if contains digits. <br /> The first element is a sign or a digit.
+ * @return int as a boolean
+ */
+int	ft_isnumber(const char *str)
+{
+	if (str[0] == '-' || str[0] == '+')
+		str++;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
