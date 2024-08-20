8e68296b87ea1bf756cc6a7538b9145e5fbb6e5a
mzurera- <mzurera-@student.42malaga.com>
2024-08-20 15:04:33 +0200

    feat(internal): Unset with realloc.

--- minishell/internal/unset_internal.c
+++ minishell/internal/unset_internal.c
@@ -12,13 +12,40 @@
 
 #include "minishell.h"
 
+/**
+ * unset_internal removes the internal entry with the given key
+ * and frees the memory allocated for it
+ * @return int true if the entry was found and removed, false otherwise
+ */
 int	unset_internal(const char *key, t_ms *ms)
 {
-	char	*value;
+	int		i;
+	int		j;
+	int		keylen;
+	int		internal_len;
+	char	***new_internal;
 
-	value = get_internal(key, ms);
-	if (value == NULL)
+	if (!key || get_internal(key, ms) == NULL)
 		return (0);
-	value[0] = '\0';
+	keylen = len(key);
+	internal_len = 0;
+	while (ms->internals && ms->internals[internal_len])
+		++internal_len;
+	new_internal = malloc(internal_len * sizeof(char **));
+	if (new_internal == NULL)
+		return (0);
+	i = 0;
+	j = 0;
+	while (ms->internals && ms->internals[j])
+	{
+		if (ft_strncmp(ms->internals[j][0], key, keylen) != 0)
+			new_internal[i++] = ms->internals[j];
+		else
+			free(ms->internals[j]);
+		++j;
+	}
+	new_internal[i] = NULL;
+	free(ms->internals);
+	ms->internals = new_internal;
 	return (1);
 }
