f3625ef27c2f4d4a5746a030bd4ad29354531e2e
mzurera- <mzurera-@student.42malaga.com>
2024-09-15 18:16:12 +0200

    refactor: builtins

--- minishell/exec/executor.c
+++ minishell/exec/executor.c
@@ -34,7 +34,7 @@ static int	execute_builtin(const t_command *command, t_ms *ms)
 	else if (ft_strncmp(command->cmd_name, "unset", 5) == 0)
 		unset(command->argv[1], ms);
 	else if (ft_strncmp(command->cmd_name, "", 1) == 0)
-		no_command()
+		no_command();
 	else
 		return (0);
 	return (1);
@@ -86,6 +86,8 @@ static void	execute_command(t_expr *expression, t_ms *ms)
 	}
 	if (execute_builtin(command, ms))
 		return ;
+	if (!find_command(&command->cmd_name))
+		return ;
 	pid = fork();
 	if (pid < 0)
 	{
@@ -93,8 +95,6 @@ static void	execute_command(t_expr *expression, t_ms *ms)
 		return ;
 	}
 	if (pid > 0)
-		return ;
-	if (!find_command(&command->cmd_name))
 		return ;
 	dup2(command->fd_in, STDIN_FILENO);
 	dup2(command->fd_out, STDOUT_FILENO);
